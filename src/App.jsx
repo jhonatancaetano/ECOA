025f36bdb4bb25f5a5cff93e1b44782e6551395f
jhonatancaetano <jhonatancaetano909@gmail.com>
2023-12-15 00:16:50 -0300

    alterações finais

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
-import React from "react";
-import AppRouter from "./routes"
+import React, {useState, useEffect} from "react";
+import AppRouter from "./routes";
+
+import api from "./Api.js"
+import axios from "axios"
 
 function App() {
+
+  useEffect(() => {
+    api.get('evento').then(res=>{
+      console.log(res.data);
+    })
+  })
+  
   return (
     <AppRouter />
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,18 @@
-import styles from "./Home.module.css"; 
-import React, { useState, useRef , useEffect} from 'react';
-import axios from 'axios';
+import styles from "./Home.module.css";
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
   const [eventos, setDados] = useState(null);
-  
+
   useEffect(() => {
     // Função para fazer a requisição ao endpoint
     async function fetchData() {
       try {
-        const response = await axios.get('http://172.105.154.34:3009/evento');
+        const response = await axios.get("http://localhost:4000/evento");
         setDados(response.data); // Define os dados recebidos no estado
       } catch (error) {
-        console.error('Erro ao obter dados:', error);
+        console.error("Erro ao obter dados:", error);
       }
     }
 
@@ -27,14 +27,16 @@ const Home = () => {
     };
 
     try {
-      const response = await axios.post('http://172.105.154.34:3009/evento', novoEvento);
-      console.log('Novo evento criado:', response.data);
+      const response = await axios.post(
+        "http://172.105.154.34:3009/evento",
+        novoEvento
+      );
+      console.log("Novo evento criado:", response.data);
       // Faça algo com a resposta, se necessário
     } catch (error) {
-      console.error('Erro ao criar novo evento:', error);
+      console.error("Erro ao criar novo evento:", error);
     }
   };
-
 
   const dateRef = useRef(null);
   const timeRef = useRef(null);
@@ -48,59 +50,57 @@ const Home = () => {
   const video = useRef(null);
   const codigoi = useRef(null);
   const codigof = useRef(null);
-  
 
   const handleDateTimeChange = () => {
     const selectedDate = dateRef.current.value;
     const selectedTime = timeRef.current.value;
-    
+
     const selectedDateTime = new Date(`${selectedDate}T${selectedTime}`);
-    alert('Data e hora de início selecionadas!', selectedDateTime);
+    alert("Data e hora de início selecionadas!", selectedDateTime);
   };
 
   const handleDateTimeChange1 = () => {
     const selectedDate1 = dateRef1.current.value;
     const selectedTime1 = timeRef1.current.value;
-    
+
     const selectedDateTime1 = new Date(`${selectedDate1}T${selectedTime1}`);
-    alert('Data e hora de fim selecionadas!', selectedDateTime1);
+    alert("Data e hora de fim selecionadas!", selectedDateTime1);
   };
 
   const handleClick1 = () => {
-    dateRef.current.value = '';
-    timeRef.current.value = '';
-    dateRef1.current.value = '';
-    timeRef1.current.value = '';
-    inputRef.current.value = '';
-    titulo.current.value = '';
-    palestrante.current.value = '';
-    local.current.value = '';
-    detalhes.current.value = '';
-    link.current.value = '';
-    video.current.value = '';
-    codigoi.current.value = '';
-    codigof.current.value = '';
-
+    dateRef.current.value = "";
+    timeRef.current.value = "";
+    dateRef1.current.value = "";
+    timeRef1.current.value = "";
+    inputRef.current.value = "";
+    titulo.current.value = "";
+    palestrante.current.value = "";
+    local.current.value = "";
+    detalhes.current.value = "";
+    link.current.value = "";
+    video.current.value = "";
+    codigoi.current.value = "";
+    codigof.current.value = "";
   };
-  
+
   const inputRef = useRef(null);
-  
+
   const handleSelectChange = (event) => {
     const selectedOption = event.target.value;
-    console.log('Opção selecionada:', selectedOption);
+    console.log("Opção selecionada:", selectedOption);
     // Faça algo com a opção selecionada, como enviar para uma função ou componente pai
   };
 
   const handleSelectChange1 = (event) => {
     const selectedOption = event.target.value;
-    console.log('Opção selecionada:', selectedOption);
+    console.log("Opção selecionada:", selectedOption);
     // Faça algo com a opção selecionada, como enviar para uma função ou componente pai
   };
 
   const handleImageUpload = (event) => {
     const selectedFile = event.target.files[0];
     // Faça o que for necessário com o arquivo selecionado (por exemplo, enviar para um servidor)
-    console.log('Arquivo selecionado:', selectedFile);
+    console.log("Arquivo selecionado:", selectedFile);
   };
 
   return (
@@ -119,7 +119,10 @@ const Home = () => {
                 src="https://cdn.animaapp.com/projects/655fc369c485099130b690f9/releases/655fc39084f33ca76ac7e557/img/arrow-left-circle-1.svg"
               />
             </a>
-            <select className={styles.overlapgroup} onChange={handleSelectChange1}>
+            <select
+              className={styles.overlapgroup}
+              onChange={handleSelectChange1}
+            >
               <option value="">Selecione</option>
               <option value="option1">1</option>
               <option value="option2">2</option>
@@ -140,21 +143,56 @@ const Home = () => {
             <div className={styles.textwrapper13}>Início</div>
             <div className={styles.textwrapper14}>Fim</div>
             <div className={styles.textwrapper15}>Número de validações</div>
-       
-            <input className={styles.rectangle} type="text" ref={titulo} placeholder="Digite aqui o título do evento" />
-           
-            <input className={styles.rectangle1} type="text" ref={palestrante} placeholder="Nome do(s) palestrante(s)" />
-         
-            <input className={styles.rectangle2} type="text" ref={local} placeholder="RDC" />
-          
-            <input className={styles.rectangle6} type="text" ref={codigoi} placeholder="Código de entrada" />
-            
-            <input className={styles.rectangle7} type="text" ref={codigof} placeholder="Código de saída" />
-            
-            <input className={styles.rectangle3} type="text" ref={link} placeholder="ecoa.puc-rio.br/eventos/21102023" />
-      
-            <input className={styles.rectangle4} type="text" ref={video} placeholder="ecoa.puc-rio.br/video/24102023" />
-            
+
+            <input
+              className={styles.rectangle}
+              type="text"
+              ref={titulo}
+              placeholder="Digite aqui o título do evento"
+            />
+
+            <input
+              className={styles.rectangle1}
+              type="text"
+              ref={palestrante}
+              placeholder="Nome do(s) palestrante(s)"
+            />
+
+            <input
+              className={styles.rectangle2}
+              type="text"
+              ref={local}
+              placeholder="RDC"
+            />
+
+            <input
+              className={styles.rectangle6}
+              type="text"
+              ref={codigoi}
+              placeholder="Código de entrada"
+            />
+
+            <input
+              className={styles.rectangle7}
+              type="text"
+              ref={codigof}
+              placeholder="Código de saída"
+            />
+
+            <input
+              className={styles.rectangle3}
+              type="text"
+              ref={link}
+              placeholder="ecoa.puc-rio.br/eventos/21102023"
+            />
+
+            <input
+              className={styles.rectangle4}
+              type="text"
+              ref={video}
+              placeholder="ecoa.puc-rio.br/video/24102023"
+            />
+
             <select className={styles.overlap8} onChange={handleSelectChange}>
               <option value="">Selecione uma opção</option>
               <option value="option1">Comunidade de Prática</option>
@@ -171,43 +209,64 @@ const Home = () => {
               <option value="option12">Conteúdos Educacionais - Química</option>
               <option value="option13">Semana PUC</option>
             </select>
-            
-            <input className={styles.rectangle8} type="text" ref={detalhes} placeholder="Descrição do evento..." />
-          
-            <input className={styles.rectangle5} type="text" ref={inputRef} placeholder="Separe as tags com vírgula" />
 
-            <input className={styles.imgfinal} type="file" accept="image/*" onChange={handleImageUpload} />
+            <input
+              className={styles.rectangle8}
+              type="text"
+              ref={detalhes}
+              placeholder="Descrição do evento..."
+            />
+
+            <input
+              className={styles.rectangle5}
+              type="text"
+              ref={inputRef}
+              placeholder="Separe as tags com vírgula"
+            />
+
+            <input
+              className={styles.imgfinal}
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+            />
 
             <div className={styles.overlap12}>
-          
               <input type="date" ref={dateRef} />
-      
-              <input type="time" ref={timeRef} />
-         
-              <button onClick={handleDateTimeChange}>Selecionar</button>
-          </div>
 
-          <div className={styles.overlap13}>
-            
-            <input type="date" ref={dateRef1} />
-    
-            <input type="time" ref={timeRef1} />
-      
-          <button onClick={handleDateTimeChange1}>Selecionar</button>
-        </div>
-            
+              <input type="time" ref={timeRef} />
+
+              <button onClick={handleDateTimeChange}>Selecionar</button>
+            </div>
+
+            <div className={styles.overlap13}>
+              <input type="date" ref={dateRef1} />
+
+              <input type="time" ref={timeRef1} />
+
+              <button onClick={handleDateTimeChange1}>Selecionar</button>
+            </div>
+
             <div className={styles.group}>
               <div className={styles.overlapgroup2}>
-                <div className={styles.textwrapper26}>Adicionar novo evento</div>
+                <div className={styles.textwrapper26}>
+                  Adicionar novo evento
+                </div>
                 <a href="http://localhost:3000/ver">
-                  <button className={styles.button1} onClick={handleSubmit}></button>
+                  <button
+                    className={styles.button1}
+                    onClick={handleSubmit}
+                  ></button>
                 </a>
               </div>
             </div>
             <div className={styles.overlapwrapper}>
               <div className={styles.overlap16}>
                 <div className={styles.textwrapper27}>Cancelar</div>
-                <button className={styles.button2} onClick={handleClick1}></button>
+                <button
+                  className={styles.button2}
+                  onClick={handleClick1}
+                ></button>
               </div>
             </div>
           </div>
@@ -215,5 +274,5 @@ const Home = () => {
       </div>
     </header>
   );
-}
+};
 export default Home;
